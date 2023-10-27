0a54d044287082a82f8a509fdbed2d86e1836d0d
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2023-10-28 01:14:44 +0530

    Delete CRUDapi directory

--- CRUDapi/src/main/java/com/apiRepository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.apiRepository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.api.entity.Employee;
-
-public interface  EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
